204a4ad2d7fbbf066afee3333345bbd07bfb4bc1
Lulily13 <bloodyakuma13@yahoo.com>
2025-05-11 15:48:18 +0200

    feat: add api and button to delete a meeting

--- src/main/frontend/src/meetings/MeetingsList.js
+++ src/main/frontend/src/meetings/MeetingsList.js
@@ -1,20 +1,28 @@
-export default function MeetingsList({meetings}) {
+// Komponent MeetingsList
+export default function MeetingsList({ meetings, onDelete }) {
     return (
         <table>
             <thead>
             <tr>
                 <th>Nazwa spotkania</th>
                 <th>Opis</th>
+                <th>Akcje</th>
             </tr>
             </thead>
             <tbody>
             {
-                meetings.map((meeting, index) => <tr key={index}>
-                    <td>{meeting.title}</td>
-                    <td>{meeting.description}</td>
-                </tr>)
+                meetings.map((meeting) => (
+                    <tr key={meeting.id}>
+                        <td>{meeting.title}</td>
+                        <td>{meeting.description}</td>
+                        <td>
+                            <button onClick={() => onDelete(meeting)}>Usuń spotkanie</button>
+                        </td>
+                    </tr>
+                ))
             }
             </tbody>
         </table>
     );
 }
+
